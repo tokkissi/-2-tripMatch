99f4a0697bc374f32734ad3edb405ef95ddafbbf
Salmambo <zxcvsqwe@gmail.com>
2022-12-13 23:14:31 +0900

    feat: mongoose로 mongodb 연결

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,12 @@
 import "dotenv/config";
+import mongoose from "mongoose";
 import app from "./app";
 
-const port = process.env.SERVER_PORT || 5000;
+const port = process.env.SERVER_PORT;
+const mongoDB = process.env.MONGODB_URI as string;
+
+mongoose.set("strictQuery", true); // strictQuery가 뭘까...
+mongoose.connect(mongoDB);
+mongoose.connection.on("connected", () => console.log("mongoDB connected"));
 
 app.listen(port, () => console.log(`server started on ${port}`));

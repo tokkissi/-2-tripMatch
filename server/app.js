7fcfffb60cce941be151cabfd9662fdd8ea38cb9
Salmambo <zxcvsqwe@gmail.com>
2022-12-13 11:41:31 +0900

    feat: 백엔드 초기 세팅

---
+++ server/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import createError from "http-errors";
+import { errorHandler } from "./middlewares";
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use((req, res, next) => next(createError(404)));
+app.use(errorHandler);
+
+export default app;

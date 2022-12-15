99dd246bcb6e2a65dae71d11fdb37eaf0ba271b1
JiYun <jo522115@gmail.com>
2022-12-15 15:00:52 +0900

    feat: footer info

--- client/src/components/MyFooter/MyFooter.tsx
+++ client/src/components/MyFooter/MyFooter.tsx
@@ -2,7 +2,27 @@ import React from "react";
 import Footer from "./MyFooterStyle";
 
 const MyFooter = () => {
-  return <Footer>푸터</Footer>;
+  return (
+    <Footer>
+      <div className="info">
+        <div className="comInfo">
+          <div>(주)TripMatch</div>
+          <div>
+            <div>대표</div>
+            <div>토끼씨</div>
+          </div>
+          <div>
+            <div>개인정보보호책임</div>
+            <div>김제원</div>
+          </div>
+          <div>국내시 동행구 여행동 82길 28</div>
+        </div>
+      </div>
+      <div className="legalInfo"></div>
+      <div className="copyright"></div>
+      <div className="snsLogo"></div>
+    </Footer>
+  );
 };
 
 export default MyFooter;
--- client/src/components/MyFooter/MyFooterStyle.tsx
+++ client/src/components/MyFooter/MyFooterStyle.tsx
@@ -6,6 +6,30 @@ const Footer = styled.footer`
   position: absolute;
   bottom: 0;
   background-color: ${(props) => props.theme.color.lightblue};
+
+  .info {
+    display: grid;
+    grid-auto-columns: 1fr;
+    grid-template-columns: 1.3fr 0.7fr;
+    grid-template-rows: 1fr 1fr 1fr;
+    gap: 0px 0px;
+    grid-template-areas:
+      "comInfo comInfo"
+      "legalInfo snsLogo"
+      "copyright snsLogo";
+  }
+  .comInfo {
+    grid-area: comInfo;
+  }
+  .snsLogo {
+    grid-area: snsLogo;
+  }
+  .legalInfo {
+    grid-area: legalInfo;
+  }
+  .copyright {
+    grid-area: copyright;
+  }
 `;
 
 export default Footer;

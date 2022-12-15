5f996ca97caaf3b4a8af616e161dfb78facc57a1
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-12-16 04:12:43 +0900

    fix: 게시글 내역 임의로 추가, top, sideBar 컴포넌트 페이지에 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home/Home";
+import MyPage from "./pages/MyPage/MyPage";
 
 const App = () => {
   return (
@@ -11,6 +12,16 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route path="" element={<Home />} />
+          <Route
+            path="mypage"
+            element={
+              <MyPage
+                title={"새해를 강릉에서! 같이가실 분..?"}
+                region={"강원도"}
+                duration={"2022.12.31 ~ 2023.01.12"}
+              />
+            }
+          />
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/pages/MyPage/myPage.tsx
+++ client/src/pages/MyPage/myPage.tsx
@@ -1,22 +1,140 @@
 import React from "react";
-import {
-  Body,
-  Container,
-  MidContainer,
-  Top,
-  SideBar,
-  Content,
-} from "./modules.style";
+import Top from "../../components/MyPage/Top/Top";
+import SideBar from "../../components/MyPage/SideBar/SideBar";
+import { Body, Container, MidContainer, Content, Layer } from "./MyPageStyle";
 
-const MyPage = () => {
+interface UserData {
+  title: any;
+  region?: any;
+  duration?: any;
+  writer?: any;
+  register?: any;
+}
+
+const MyPage: React.FC<UserData> = ({ title, region, duration }) => {
   return (
     <>
       <Body>
         <Container>
-          <Top>Top</Top>
+          <Top name={"doylee"} tripCount={32} score={4.5} />
           <MidContainer>
-            <SideBar>side-bar</SideBar>
-            <Content>Body</Content>
+            <SideBar />
+            <Content>
+              <Layer>
+                <table>
+                  <thead>
+                    <tr id="first">
+                      <th>제목</th>
+                      <th>지역</th>
+                      <th>여행기간</th>
+                      <th>상태</th>
+                    </tr>
+                  </thead>
+
+                  <tbody>
+                    <tr>
+                      <td>{title}</td>
+                      <td>{region}</td>
+                      <td>{duration}</td>
+                      <td id="last">
+                        <select name="" id="">
+                          <option value="모집중">모집중</option>
+                          <option value="모집마감">모집마감</option>
+                        </select>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>{title}</td>
+                      <td>{region}</td>
+                      <td>{duration}</td>
+                      <td id="last">
+                        <select name="" id="">
+                          <option value="모집중">모집중</option>
+                          <option value="모집마감">모집마감</option>
+                        </select>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>{title}</td>
+                      <td>{region}</td>
+                      <td>{duration}</td>
+                      <td id="last">
+                        <select name="" id="">
+                          <option value="모집중">모집중</option>
+                          <option value="모집마감">모집마감</option>
+                        </select>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>{title}</td>
+                      <td>{region}</td>
+                      <td>{duration}</td>
+                      <td id="last">
+                        <select name="" id="">
+                          <option value="모집중">모집중</option>
+                          <option value="모집마감">모집마감</option>
+                        </select>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>{title}</td>
+                      <td>{region}</td>
+                      <td>{duration}</td>
+                      <td id="last">
+                        <select name="" id="">
+                          <option value="모집중">모집중</option>
+                          <option value="모집마감">모집마감</option>
+                        </select>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>{title}</td>
+                      <td>{region}</td>
+                      <td>{duration}</td>
+                      <td id="last">
+                        <select name="" id="">
+                          <option value="모집중">모집중</option>
+                          <option value="모집마감">모집마감</option>
+                        </select>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>{title}</td>
+                      <td>{region}</td>
+                      <td>{duration}</td>
+                      <td id="last">
+                        <select name="" id="">
+                          <option value="모집중">모집중</option>
+                          <option value="모집마감">모집마감</option>
+                        </select>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>{title}</td>
+                      <td>{region}</td>
+                      <td>{duration}</td>
+                      <td id="last">
+                        <select name="" id="">
+                          <option value="모집중">모집중</option>
+                          <option value="모집마감">모집마감</option>
+                        </select>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>{title}</td>
+                      <td>{region}</td>
+                      <td>{duration}</td>
+                      <td id="last">
+                        <select name="" id="">
+                          <option value="모집중">모집중</option>
+                          <option value="모집마감">모집마감</option>
+                        </select>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </Layer>
+            </Content>
           </MidContainer>
         </Container>
       </Body>

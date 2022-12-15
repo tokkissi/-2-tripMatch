aa3ba7f6ee1888b15e4c291bdd019f5c1cf488cb
JiYun <jo522115@gmail.com>
2022-12-15 16:52:24 +0900

    feat: footer UI 완성

--- client/src/components/MyFooter/MyFooter.tsx
+++ client/src/components/MyFooter/MyFooter.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Footer from "./MyFooterStyle";
+import insta from "../../images/freeIconInstagram.png";
+import twit from "../../images/freeIconTwitter.png";
+import face from "../../images/freeIconFacebook.png";
 
 const MyFooter = () => {
   return (
@@ -8,19 +11,33 @@ const MyFooter = () => {
         <div className="comInfo">
           <div>(주)TripMatch</div>
           <div>
-            <div>대표</div>
-            <div>토끼씨</div>
+            <span>대표</span>
+            <span>토끼씨</span>
           </div>
           <div>
-            <div>개인정보보호책임</div>
-            <div>김제원</div>
+            <span>개인정보보호책임</span>
+            <span>김제원</span>
           </div>
           <div>국내시 동행구 여행동 82길 28</div>
         </div>
+        <div className="legalInfo">
+          <span>개인정보처리방침</span>
+          <span>|</span>
+          <span>이용약관</span>
+          <span>|</span>
+          <span>취소 및 환불정책</span>
+          <span>|</span>
+          <span>파트너 입점 문의</span>
+        </div>
+        <div className="copyright">
+          Copyright © TRIPMATCH Inc. All Rights Reserved.
+        </div>
+        <div className="snsLogo">
+          <img src={insta} />
+          <img src={twit} />
+          <img src={face} />
+        </div>
       </div>
-      <div className="legalInfo"></div>
-      <div className="copyright"></div>
-      <div className="snsLogo"></div>
     </Footer>
   );
 };

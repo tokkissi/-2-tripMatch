e8074843607ac7edd64c704e0b57fea1ceddcacb
younngg <8533283@naver.com>
2022-12-16 14:19:03 +0900

    fix: dummy.png import 오류 해결

--- client/src/pages/MatchPostDetail/MatchPostDetail.tsx
+++ client/src/pages/MatchPostDetail/MatchPostDetail.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 import PostDetail from "./../../components/PostDetail/PostDetail";
-import dummy from "../dummy.png";
 import Comment from "../../components/Comment/Comment";
 
 const MatchPostDetail = () => {
@@ -12,7 +11,7 @@ const MatchPostDetail = () => {
     region: "경상도",
     duration: ["2022-12-14", "2022-12-15"],
     userCount: 4,
-    thumbnailImg: dummy,
+    thumbnailImg: "",
   };
 
   const userData = {

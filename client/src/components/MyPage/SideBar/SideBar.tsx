abef7675279188a543d4d81abea82a281691c269
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-12-19 14:22:00 +0900

    fix: sideBar link 연결

--- client/src/components/MyPage/SideBar/SideBar.tsx
+++ client/src/components/MyPage/SideBar/SideBar.tsx
@@ -1,50 +1,28 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { SideBar, Box } from "./SideBarStyle";
 
 const SideBarComponent: React.FC = () => {
-  const [color, setColor] = useState<boolean>(false);
+  // const [color, setColor] = useState<boolean>(false);
+
+  // className={color ? "color" : ""} onClick={() => setColor(!color)}
+
+  // const colorHandler = (e: any) => {
+  //   e.preventDefault();
+  //   setColor(!color);
+  // };
+
   return (
     <SideBar>
-      <Box
-        value="정보 수정"
-        className={color ? "color" : ""}
-        onClick={() => setColor(!color)}
-      >
-        정보 수정
-      </Box>
+      <Box value="정보 수정">정보 수정</Box>
       <Link to="/mypage">
-        <Box
-          value="게시글 내역"
-          className={color ? "color" : ""}
-          onClick={() => setColor(!color)}
-        >
-          게시글 내역
-        </Box>
+        <Box value="게시글 내역">게시글 내역</Box>
       </Link>
       <Link to="/myComment">
-        <Box
-          value="댓글 내역"
-          className={color ? "color" : ""}
-          onClick={() => setColor(!color)}
-        >
-          댓글 내역
-        </Box>
+        <Box value="댓글 내역">댓글 내역</Box>
       </Link>
-      <Box
-        value="신청받은 내역"
-        className={color ? "color" : ""}
-        onClick={() => setColor(!color)}
-      >
-        신청받은 내역
-      </Box>
-      <Box
-        value="신청한 내역"
-        className={color ? "color" : ""}
-        onClick={() => setColor(!color)}
-      >
-        신청한 내역
-      </Box>
+      <Box value="신청받은 내역">신청받은 내역</Box>
+      <Box value="신청한 내역">신청한 내역</Box>
     </SideBar>
   );
 };

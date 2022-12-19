79e0694efada89dbe94a3fa65b2670f3566a3554
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2022-12-19 14:47:40 +0900

    20221219 2주차 FE Free Post 수정
    ## merge request 내역
    
    - merge 요청자: @eunchae
    
    ## 1. 어떤 이유로 MR를 하셨나요?
    ### merge request할 때, commit한 이유를 리스트에서 체크하고 설명해주세요.
    | MR 이유 | 해당 사항 체크(✅,❎) |
    | ------ | ------ |
    | feature 병합 |  |
    | 버그 수정 |  |
    | 코드 개선 |  |
    | 기존 기능 손상 -> 업데이트 필요 |  |
    | 기타 (아래에 자세한 내용을 기입해주세요) | ✅ |
    - 자세한 내용 :
    
    ## 2. 변경 사항이 무엇인가요?
    
    - FreePost -> FreePostList 로 변경
    - FreePostList 스타일 변경

--- client/src/components/FreePost/FreePostList.tsx
+++ client/src/components/FreePost/FreePostList.tsx
@@ -13,10 +13,10 @@ import {
   CommentInfo,
   CommentImage,
   CommentCount,
-} from "./FreePostRowStyle";
+} from "./FreePostListStyle";
 import CommentLogo from "../../images/comment-dots.png";
 
-const FreePostRow = () => {
+const FreePost = () => {
   const mockData = [
     {
       postID: "1",
@@ -32,12 +32,14 @@ const FreePostRow = () => {
       nickname: "가나다라",
       region: "충청도",
       title: "제목입니다",
+      category: "숙소",
       createdAt: "22.12.12",
       commentCount: 3,
     },
     {
       postID: "3",
       nickname: "가나다라",
+      category: "숙소",
       region: "충청도",
       title: "제목입니다",
       createdAt: "22.12.12",
@@ -46,6 +48,7 @@ const FreePostRow = () => {
     {
       postID: "4",
       nickname: "가나다라",
+      category: "숙소",
       region: "충청도",
       title: "제목입니다",
       createdAt: "22.12.12",
@@ -54,6 +57,7 @@ const FreePostRow = () => {
     {
       postID: "5",
       nickname: "가나다라",
+      category: "숙소",
       region: "충청도",
       title: "제목입니다",
       createdAt: "22.12.12",
@@ -62,6 +66,7 @@ const FreePostRow = () => {
     {
       postID: "6",
       nickname: "가나다라",
+      category: "숙소",
       region: "충청도",
       title: "제목입니다",
       createdAt: "22.12.12",
@@ -71,24 +76,30 @@ const FreePostRow = () => {
 
   return (
     <Container>
-      <MainContent>
-        <PostInfo>
-          <Region>[부산]</Region>
-          <Category>[숙소]</Category>
-          <Title>부산 맛집 추천받습니다</Title>
-        </PostInfo>
-        <UserInfo>
-          <Nickname>USER123</Nickname>
-          <SeparateLine>|</SeparateLine>
-          <CreatedDate>22.12.12</CreatedDate>
-        </UserInfo>
-      </MainContent>
-      <CommentInfo>
-        <CommentImage src={CommentLogo} alt="" />
-        <CommentCount>3</CommentCount>
-      </CommentInfo>
+      {mockData.map((data, i) => {
+        return (
+          <div className="container" key={i}>
+            <MainContent>
+              <PostInfo>
+                <Region>[{data.region}]</Region>
+                <Category>[{data.category}]</Category>
+                <Title>{data.title}</Title>
+              </PostInfo>
+              <UserInfo>
+                <Nickname>{data.nickname}</Nickname>
+                <SeparateLine>|</SeparateLine>
+                <CreatedDate>{data.createdAt}</CreatedDate>
+              </UserInfo>
+            </MainContent>
+            <CommentInfo>
+              <CommentImage src={CommentLogo} alt="" />
+              <CommentCount>{data.commentCount}</CommentCount>
+            </CommentInfo>
+          </div>
+        );
+      })}
     </Container>
   );
 };
 
-export default FreePostRow;
+export default FreePost;

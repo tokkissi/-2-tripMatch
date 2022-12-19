70a526ad2119a9a79bba742d86072168c439c8c8
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2022-12-20 00:36:50 +0900

    20221219 2주차 FE Select 컴포넌트 생성
    - merge 요청자: @eunchae
    
    | MR 이유 | 해당 사항 체크(✅,❎) |
    | ------ | ------ |
    | feature 병합 |  |
    | 버그 수정 |  |
    | 코드 개선 |  |
    | 기존 기능 손상 -> 업데이트 필요 |  |
    | 기타 (아래에 자세한 내용을 기입해주세요) | ✅ |
    - 자세한 내용 :
    
    - Select 컴포넌트 생성

---
+++ client/src/components/AppSelect/AppSelectStyle.tsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import Theme from "../../styles/Theme";
+
+export const Div = styled.div`
+  margin: 10px 0;
+  display: flex;
+`;
+
+export const Select = styled.select`
+  border: 1px solid #dbdbdb;
+  border-radius: 5px;
+  color: #333;
+  width: 100px;
+  height: 40px;
+  outline: none;
+  text-indent: 10px;
+
+  &:focus {
+    outline: none;
+    border: 2px solid ${Theme.color.blue};
+  }
+`;
+
+export const Label = styled.label`
+  align-self: center;
+  margin-right: 15px;
+`;

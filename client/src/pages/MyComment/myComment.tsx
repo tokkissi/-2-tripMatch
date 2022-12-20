68de48cffd1d96856b076f066035b2e2f9af7885
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-12-20 19:12:22 +0900

    fix: 파일 이름 수정

--- client/src/pages/MyComment/myComment.tsx
+++ client/src/pages/MyComment/myComment.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Top from "../MyPage/components/Top";
 import SideBar from "../MyPage/components/SideBar";
 import MyCommentTable from "../MyPage/components/MyCommentTable";
-import { Body, Container, MidContainer } from "./myCommentStyle";
+import { Body, Container, MidContainer } from "./MyCommentStyle";
 
 const MyComment: React.FC = () => {
   return (

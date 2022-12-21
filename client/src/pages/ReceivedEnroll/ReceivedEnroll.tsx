dac1048192aaeefac753ec4ae1600cec215cf598
Doyeon Lee <99410440+leedo97y@users.noreply.github.com>
2022-12-21 16:34:26 +0900

    style: 반복되는 style 코드 파일 하나만 남기고 정리

--- client/src/pages/ReceivedEnroll/ReceivedEnroll.tsx
+++ client/src/pages/ReceivedEnroll/ReceivedEnroll.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Top from "../MyPage/components/Top";
 import SideBar from "../MyPage/components/SideBar";
-import { Body, Container, MidContainer } from "./ReceivedEnrollStyle";
+import { Body, Container, MidContainer } from "../MyPage/MyPageContentsStyle";
 import ReceivedEnrollTable from "./ReceivedEnrollTable";
 
 const ReceivedEnroll: React.FC = () => {

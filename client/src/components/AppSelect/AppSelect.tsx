8308f4d6025ce59cdea5f124bc66142f83a0895f
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2022-12-22 00:48:18 +0900

    20221222 2주차 FE AppSelect onChange props 추가
    - merge 요청자: @eunchae
    
    | MR 이유 | 해당 사항 체크(✅,❎) |
    | ------ | ------ |
    | feature 병합 |  |
    | 버그 수정 |  |
    | 코드 개선 |  |
    | 기존 기능 손상 -> 업데이트 필요 |  |
    | 기타 (아래에 자세한 내용을 기입해주세요) | ✅ |
    - 자세한 내용 :
    -  AppSelect onChange props 추가

--- client/src/components/AppSelect/AppSelect.tsx
+++ client/src/components/AppSelect/AppSelect.tsx
@@ -5,13 +5,19 @@ interface AppSelectProps {
   label?: string;
   options: string[];
   className: string;
+  onChange?: React.ChangeEventHandler<HTMLSelectElement>;
 }
 
-const AppSelect: React.FC<AppSelectProps> = ({ label, options, className }) => {
+const AppSelect: React.FC<AppSelectProps> = ({
+  label,
+  options,
+  className,
+  onChange,
+}) => {
   return (
     <Div>
       {label && <Label htmlFor={className}>{label}</Label>}
-      <Select className={className}>
+      <Select className={className} onChange={onChange}>
         {options.map((option, idx) => (
           <option key={idx} value={option}>
             {option}

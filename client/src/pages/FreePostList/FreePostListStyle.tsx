5f09fdbd9aa55b21923eb4cd5c462013dd7dc364
EUNCHAE KIM <112812483+eunbbori@users.noreply.github.com>
2022-12-22 19:55:10 +0900

    20221222 2주차 동행게시판, 자유게시판에 FE Button 컴포넌트 추가
    - merge 요청자: @eunchae
    
    | MR 이유 | 해당 사항 체크(✅,❎) |
    | ------ | ------ |
    | feature 병합 |  |
    | 버그 수정 |  |
    | 코드 개선 |  |
    | 기존 기능 손상 -> 업데이트 필요 |  |
    | 기타 (아래에 자세한 내용을 기입해주세요) | ✅ |
    - 자세한 내용 :
    
    동행게시판, 자유게시판에  Button 컴포넌트 추가

--- client/src/pages/FreePostList/FreePostListStyle.tsx
+++ client/src/pages/FreePostList/FreePostListStyle.tsx
@@ -3,3 +3,10 @@ import styled from "styled-components";
 export const Container = styled.div`
   padding: 50px 100px;
 `;
+
+export const ButtonContainer = styled.div`
+  width: 1000px;
+  display: flex;
+  margin-inline: auto;
+  justify-content: flex-end;
+`;
